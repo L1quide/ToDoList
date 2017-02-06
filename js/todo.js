b4d61a8c9b89dc6de7334b654412b7cafe8f104c
L1quide <xervam111@inbox.ru>
2017-02-06 17:48:54 +0700

    qqq

---
+++ js/todo.js
@@ -0,0 +1,41 @@
+﻿var app = angular.module('task', []);
+
+app.controller('TaskController', function($scope) {
+
+    $scope.saved = localStorage.getItem('tasks');
+
+    $scope.tasks = (localStorage.getItem('tasks') !== null) ?
+        JSON.parse($scope.saved) : [{
+        text: "todo",
+        complete: false
+    }];
+    localStorage.setItem('tasks', JSON.stringify($scope.tasks));
+
+    $scope.addTask = function() {
+        if ($scope.newtask) {
+            $scope.tasks.push({
+                text: $scope.newtask,
+                complete: false
+            });
+        };
+        $scope.newtask = '';
+
+        localStorage.setItem('tasks', JSON.stringify($scope.tasks));
+    };
+
+    $scope.clearComplete = function() {
+
+        var completedTask = $scope.tasks;
+        $scope.tasks = [];
+        angular.forEach(completedTask, function(tasks) {
+            if (!tasks.complete) {
+                $scope.tasks.push(tasks);
+            }
+        });
+        localStorage.setItem('tasks', JSON.stringify($scope.tasks));
+    };
+
+    $scope.save = function() {
+        localStorage.setItem('tasks', JSON.stringify($scope.tasks));
+    };
+});
